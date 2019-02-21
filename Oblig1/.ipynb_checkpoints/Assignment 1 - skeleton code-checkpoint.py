f4d54f767681d6e9ee7c87feabaea4ab1aab966b
Lars-Eskild Jahnke <lars-eskild.jahnke@student.uib.no>
2019-02-21 16:39:18 +0100

    comments

--- Oblig1/.ipynb_checkpoints/Assignment 1 - skeleton code-checkpoint.py
+++ Oblig1/.ipynb_checkpoints/Assignment 1 - skeleton code-checkpoint.py
@@ -4,62 +4,39 @@ import sys # In order to terminate the program
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
 #Prepare a sever socket
-#Fill in start
-
-
 serverSocket.bind(('', 8000))
 serverSocket.listen()
 
-
-#Fill in end
-
-
 while True:
+    
     #Establish the connection
     print('Ready to serve...')
-    
-    #Fill in start
     connectionSocket, addr = serverSocket.accept()
-    #Fill in end          
+    
     try:
-        #Fill in start 
+        
+        #Receive the request from socket
         message = connectionSocket.recv(1024).decode()
+        
+        #Check for empty requests (Google Chrome apparently does this :< )
         if len(message) > 0:
-            #Fill in end
             filename = message.split()[1]                 
-            f = open(filename[1:])
-
-            #Fill in start  
+            f = open(filename[1:]) 
             outputdata = f.read()
             f.close
-            #Fill in end   
-
+            
             #Send one HTTP header line into socket
-            #Fill in start
             connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
-            #Fill in end
-
+            
             #Send the content of the requested file to the client
             for i in range(0, len(outputdata)):           
                 connectionSocket.send(outputdata[i].encode())
             connectionSocket.send("\r\n".encode())
         connectionSocket.close()
     except IOError:
+        
         #Send response message for file not found
-        #Fill in start
         connectionSocket.send('HTTP/1.1 404 Not Found\r\n\r\n<!DOCTYPE html><html><head><meta charset="utf-8"><title>404 page not found</title></head><body><h1>404 nothing too see here</h1></body></html>'.encode())
-        #Fill in end
-        #Close client socket
-        #Fill in start
-        #Fill in end
+        connectionSocket.close()
 connectionSocket.close()
-
-sys.exit() #Terminate the program after sending the corresponding data                                    
-
-
-
-
-
-
-
-
+sys.exit() #Terminate the program after sending the corresponding data
