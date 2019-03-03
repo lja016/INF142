8df825eea4326471359acfb201eb3ffba522ec72
Lars-Eskild Jahnke <lars-eskild.jahnke@student.uib.no>
2019-03-03 17:50:37 +0100

    client working

--- Oblig1/client.py
+++ Oblig1/client.py
@@ -4,11 +4,24 @@ import sys # In order to terminate the program
 
 input = (sys.argv)
 server_host = input[1]
-server_port = input[2]
+server_port = int(input[2])
 filename = input[3]
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((server_host, server_port))
-    s.sendall('GET /' + filename + ' HTTP1.1\r\n\r\n')
-    data = s.recv(1024)
-print('Received', repr(data))
+
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+server_address = (server_host, server_port)
+client_socket.connect(server_address)
+
+request_header = ('GET /' + filename + ' HTTP1.1\r\n\r\n')
+client_socket.send(request_header.encode())
+
+response = ''
+while True:
+    recv = client_socket.recv(1024).decode()
+    if not recv:
+        break
+    response += recv 
+
+print(response)
+client_socket.close()    
